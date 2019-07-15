35e7e520e9f2bcca4c2dea2da44a8efe441035ff
sithi5 <ma.sithis@gmail.com>
2019-07-15 19:55:43 +0200

    minor change and test

---
+++ index.js
@@ -0,0 +1,13 @@
+var idx = 1;
+
+function Hide_button() {
+    if (idx == 1) {
+        console.log("Hidding button");
+        document.getElementById("button-container").style.display = "none";
+        idx = 2;
+    } else {
+        console.log("Showing button");
+        document.getElementById("button-container").style.display = "block";
+        idx = 1;
+    }
+}
--- index.php
+++ index.php
@@ -1,39 +1,57 @@
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Mes projets Web</title>
-	</head>
-	<body>
-		<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
-	<button onclick="window.location.href = 'menu.html';">menu des roulants</button>
-	<button onclick="window.location.href = 'inscription.html';">inscription</button>
-	<button onclick="window.location.href = 'Camagru.php';">Camagru</button>
-	<?php
-		try
-		{
-			$db = new PDO('mysql:host=localhost;dbname=Camagru;charset=utf8', 'julien', 'root');
-		}
-		// Si on y arrive pas
-		catch(Exception $e)
-		{
-			die('Erreur : '.$e->getMessage());
-		}
-		// REquete SQL, on selectionne tout
-		$reponse = $db->query('SELECT * FROM User');
-		// On affiche  le tout avec le while et du html
-		while ($donnees = $reponse->fetch()) // recup sous formne de tab les donnes de la table
-		{
-	?>
-		<p>
-			<strong>ID</strong> : <?php echo $donnees['id']; ?><br />
-			user : <?php echo $donnees['login']; ?> <br />
-			email :  <?php echo $donnees['mail']; ?> <br />
-			passwd <?php echo $donnees['passwd']; ?> <br />
-			<?php if ($donnees['root'] === "1") echo "Utilisateur Root" ?> <br />
-		</p>
-	<?php
-		}
-	?>
+<head>
+	<meta charset="UTF-8">
+	<title>Mes projets Web</title>
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
 
-	</body>
+<body>
+	<p id="date"> Date :<?php echo date('d/m/Y h:i:s'); ?>.</p>
+	<div class="slide-container">
+	</div>
+
+	<div id="button-container">
+		<button class="button" id="boutton-menu-des-roulants" type="button" onclick="window.location.href = 'menu.html';">menu des roulants</button>
+		<button class="button" id="boutton-inscription" type="button" onclick="window.location.href = 'inscription.html';">inscription</button>
+		<button class="button" id="boutton-camagru" type="button" onclick="window.location.href = 'Camagru.php';">Camagru</button>
+	</div>
+	<button class="button" type="button" onclick="Hide_button()">Show/Hide Buttons</button>
+
+</body>
 </html>
+
+<script src="index.js"></script>
+
+
+
+
+
+
+
+
+
+
+<?php
+try {
+	$db = new PDO('mysql:host=localhost;dbname=Camagru;charset=utf8', 'julien', 'root');
+}
+// Si on y arrive pas
+catch (Exception $e) {
+	die('Erreur : ' . $e->getMessage());
+}
+// REquete SQL, on selectionne tout
+$reponse = $db->query('SELECT * FROM User');
+// On affiche  le tout avec le while et du html
+while ($donnees = $reponse->fetch()) // recup sous formne de tab les donnes de la table
+{
+	?>
+	<p>
+		<strong>ID</strong> : <?php echo $donnees['id']; ?><br />
+		user : <?php echo $donnees['login']; ?> <br />
+		email : <?php echo $donnees['mail']; ?> <br />
+		passwd <?php echo $donnees['passwd']; ?> <br />
+		<?php if ($donnees['root'] === "1") echo "Utilisateur Root" ?> <br />
+	</p>
+<?php
+}
+?>

12d4b9cc7eb51f63cd215928051b5c3e1c663577
ChokMania <judumay@student.42.fr>
2019-07-23 18:28:21 +0200

    change connexion

--- connexion.php
+++ connexion.php
@@ -15,42 +15,42 @@ if (isset($_POST) && !empty($_POST)) {
 	extract($_POST);
 	$login = htmlentities(trim($login));
 	$password = shamalo(htmlentities(trim($password)));
-}
 //verify if login and password are provided
-if (empty($login) || empty($password)) {
-	$valid = false;
-	if (empty($login)) {
-		$er_login = ("Le login ne peut pas être vide");
-	} else {
-		$er_password = ("Le mot de passe ne peut pas être vide");
-	}
-} else {
-	$sql = "SELECT `login`, id, pwd, `super-root` FROM user WHERE `login` = :login";
-	$stmt = $db->prepare($sql);
-
-	//Bind value.
-	$stmt->bindValue(':login', $login);
-
-	//Execute.
-	$stmt->execute();
-
-	//Fetch row return the result of the sql request as an array. NULL if no result.
-	$user = $stmt->fetch(PDO::FETCH_ASSOC);
-	if ($user === false) {
-		$er_login = ("Le login ne correspond pas a un utilisateur inscrit");
-	} else {
-		if ($user['pwd'] === $password) {
-			$_SESSION['id'] = $user['id'];
-			$_SESSION['user_login'] = $user['login'];
-			$_SESSION['loggued_on'] = "1";
-			$_SESSION['logged_in'] = time();
-			if ($user['super-root'] == 1) {
-				$_SESSION['sa'] = "1";
-			}
-			header('Location: ./');
-			exit();
+	if (empty($login) || empty($password)) {
+		$valid = false;
+		if (empty($login)) {
+			$er_login = ("Le login ne peut pas être vide");
 		} else {
-			$er_password = ("Le mot de passe ne correspond pas au login");
+			$er_password = ("Le mot de passe ne peut pas être vide");
+		}
+	} else {
+		$sql = "SELECT `login`, id, pwd, `super-root` FROM user WHERE `login` = :login";
+		$stmt = $db->prepare($sql);
+
+		//Bind value.
+		$stmt->bindValue(':login', $login);
+
+		//Execute.
+		$stmt->execute();
+
+		//Fetch row return the result of the sql request as an array. NULL if no result.
+		$user = $stmt->fetch(PDO::FETCH_ASSOC);
+		if ($user === false) {
+			$er_login = ("Le login ne correspond pas a un utilisateur inscrit");
+		} else {
+			if ($user['pwd'] === $password) {
+				$_SESSION['id'] = $user['id'];
+				$_SESSION['user_login'] = $user['login'];
+				$_SESSION['loggued_on'] = "1";
+				$_SESSION['logged_in'] = time();
+				if ($user['super-root'] == 1) {
+					$_SESSION['sa'] = "1";
+				}
+				header('Location: ./');
+				exit();
+			} else {
+				$er_password = ("Le mot de passe ne correspond pas au login");
+			}
 		}
 	}
 }

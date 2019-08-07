175cf85a4be5dbc0e41657767c5aa91b79b61b6b
sithi5 <ma.sithis@gmail.com>
2019-08-07 17:58:21 +0200

    minor fix

---
+++ config/dropdb.php
@@ -0,0 +1,23 @@
+<?Php
+require 'database.php';
+
+
+$dns = $dsn = "mysql:host=".$DB_HOST;
+try {
+	$db = new PDO($dsn, $DB_USER, $DB_PASSWORD);
+}
+catch (Exception $e) {
+	print "Error message :\t" . $e->getMessage() . "\n";
+	exit();
+}
+echo("connection to db ok!\n");
+
+
+$sql = "DROP DATABASE IF EXISTS " . "$DB_NAME";
+
+//Prepare the SQL query.
+$statement = $db->prepare($sql);
+//Execute the statement.
+$statement->execute();
+
+?>

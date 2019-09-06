33f4a2fc0c098f09e37d62970090836dd408fc6e
Julien DUMAY <judumay@e1r4p13.42.fr>
2019-09-06 12:20:09 +0200

    maj responsive

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-	$DB_HOST = '127.0.0.1:8889';//8889 // 3306 // local 127.0.0.1
+	$DB_HOST = '127.0.0.1:3306';//8889 // 3306 // local 127.0.0.1
 	$DB_NAME = 'Camagru';
 	$DB_DSN = "mysql:host=".$DB_HOST.";dbname=".$DB_NAME;
 	$DB_USER = 'root';
-	$DB_PASSWORD = 'root';
+	$DB_PASSWORD = 'rootroot';
 ?>

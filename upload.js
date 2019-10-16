ee5e67f0b593a670b048cfcc2002a79ece120ef4
Julien DUMAY <judumay@e2r10p21.42.fr>
2019-10-16 14:27:41 +0200

    upload.js

--- upload.js
+++ upload.js
@@ -48,19 +48,22 @@ window.addEventListener("load", function(){
 					} else {
 						hello = this.response;
 						var xhrq = new XMLHttpRequest();
-						if (typeof src === "undifined"){
-							;
+						if (typeof(src) === 'undefined' ){
+							alert("ici");
+							src = "";
+							xhrq.open('GET', "./filtre.php?img=" + hello + "&filtre=0", true);
 						}
-						var words = src.split('/');
-						src = "";
-						console.log("http://localhost:8081/filtre.php?img=" + hello + "&filtre=./ressources/filters/" + words[5]);
-						xhrq.open('GET', "http://localhost:8081/filtre.php?img=" + hello + "&filtre=./ressources/filters/" + words[5], true);
-						xhrq.send(null);
-						xhrq.onload = function(){
-							if (xhr.status==403 || xhr.status==404) {
-								alert("ERROR LOADING UPLOAD.PHP");
-							} else {
-								console.log(this.response);
+						else {
+							var words = src.split('/');
+							console.log("./filtre.php?img=" + hello + "&filtre=./ressources/filters/" + words[5]);
+							xhrq.open('GET', "./filtre.php?img=" + hello + "&filtre=./ressources/filters/" + words[5], true);
+							xhrq.send(null);
+							xhrq.onload = function(){
+								if (xhr.status==403 || xhr.status==404) {
+									alert("ERROR LOADING UPLOAD.PHP");
+								} else {
+									console.log(this.response);
+								}
 							}
 						}
 					}

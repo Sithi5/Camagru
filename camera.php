12b14f279c1897dbd563815ded9c6243486142c1
Mabouce <mabouce@student.42.fr>
2019-10-16 14:14:11 +0200

    adding the cam and css

--- camera.php
+++ camera.php
@@ -2,85 +2,36 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="./css/camera.css">
 </head>
-<div class="parent_div_container_1">
-	<div class="child-1">
-		<?php $filtre = "" ?>
-		<div id="cssfilters">
-			<div class="child-1-1" ondrop="drop(event)" ondragover="allowDrop(event)">
-				<video class="videostream" autoplay></video>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<script src="upload.js"></script>
+</head>
+	<body>
+		<div class="parent_div_container_3">
+			<!-- left part of modal-->
+			<div class="child-camera-1">
+				<div id="vid-controls">
+					<div style="width: 500px;height:370px;border: solid 1px black"ondrop="drop(event)" ondragover="allowDrop(event)">
+					<video class="vidclass" id="vid-show" autoplay></video></div>
+					<center>
+					<input id="vid-take" type="button" class="btn" value="Take Photo"/>
+					</center>
+					<div id="vid-canvas"></div>
+				</div>
 			</div>
-			<div class="child-1-1">
-				<img class="screenshot" id="screenshot-img">
-			</div>
-			<center>
-			<p><button class="capture-button btncapt">Capture video</button></p>
-				<p><button class="btncapt" id="screenshot-button" disabled>Take screenshot</button></p>
-			</center>
+			<!-- right part of modal-->
+			<div class="child-camera-2">
+			<?php
+				$filter = -3;
+				$d = dir("./ressources/filters/.");
+				while (false !== ($entry = $d->read())) {
+					if (++$filter > 0) {
+			?>
+					<img style="width:100px;height:100px"id="<?php $result = "f" . $filter;
+						echo $result ?>" class="filter-img" onclick="" href="#" draggable="true" ondragstart="drag(event)" src="../ressources/filters/<?=$entry?>" alt="filter">
+				<?php
+					}
+				}
+				$d->close();
+				?>
 		</div>
 	</div>
-	<div class="child-2" id="container">
-		<?php $filter = 0 ?>
-		<img id="<?php $result = "f" . $filter++;
-					echo $result ?>" class="filter-img" onclick="" href="#" draggable="true" ondragstart="drag(event)" src="./ressources/filters/sombrero.png" alt="filter">
-		<img id="<?php $result = "f" . $filter++;
-					echo $result ?>" class="filter-img" onclick="" href="#" draggable="true" ondragstart="drag(event)" src="./ressources/filters/like.png" alt="filter">
-
-</div>
-</div>
-
-
-<!--script part-->
-<script>
-	function handleError(error) {
-		console.error('navigator.getUserMedia error: ', error);
-	}
-	const constraints = {
-		video: true
-	};
-	(function() {
-		const captureVideoButton = document.querySelector('#cssfilters .capture-button');
-		const video = document.querySelector('#cssfilters video');
-		const screenshotButton = document.querySelector('#screenshot-button');
-		const img = document.querySelector('#screenshot-img');
-		const canvas = document.createElement('canvas');
-
-		screenshotButton.onclick = video.onclick = function() {
-			canvas.width = video.videoWidth;
-			canvas.height = video.videoHeight;
-			canvas.getContext('2d').drawImage(video, 0, 0);
-			// Other browsers will fall back to image/png
-			img.src = canvas.toDataURL('image/png');
-		};
-
-		captureVideoButton.onclick = function() {
-			navigator.mediaDevices.getUserMedia(constraints).
-			then(handleSuccess).catch(handleError);
-		};
-		function handleSuccess(stream) {
-			screenshotButton.disabled = false;
-			localMediaStream = stream;
-			video.srcObject = stream;
-		}
-	})();
-
-
-
-
-	//////////////////////////////// drag n drop function
-
-	function allowDrop(ev) {
-		ev.preventDefault();
-	}
-
-	function drag(ev) {
-		ev.dataTransfer.setData("text", ev.target.id);
-	}
-
-	function drop(ev) {
-		ev.preventDefault();
-		var data = ev.dataTransfer.getData("text");
-		console.log("filter " + data + " dropped");
-	}
-	///////////end of drag n drop funct
-
-</script>

f0d869a4d04c9fdcb631c8842778a12c9dde43f0
Mabouce <mabouce@student.42.fr>
2019-10-17 12:00:51 +0200

    minor change

--- post.php
+++ post.php
@@ -92,5 +92,5 @@
 		</div>
 	</body>
 </html>
-<script src="./ajax.js"></script>
+<script src="./script/ajax.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>

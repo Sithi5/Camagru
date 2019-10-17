7ebd5cbeb27c4970bb383fe19bcaecc71a85b208
Julien DUMAY <judumay@e2r10p12.42.fr>
2019-10-17 13:52:44 +0200

    push nouveau filtre

--- filtre.php
+++ filtre.php
@@ -8,9 +8,9 @@
 		$height = 480;
 		list($newwidth, $newheight) = getimagesize($_GET['filtre']);
 		$out = imagecreatetruecolor($width, $height);
-		imagecopyresampled($out, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
-		imagecopyresampled($out, $png, 0, 0, 0, 0, $width, $height, $newwidth, $newheight);
+		imagecopyresampled($out, $jpeg, 0, 0, 100, 0, $width, $height, $width, $height);
 		imageflip($out, IMG_FLIP_HORIZONTAL);
+		imagecopyresampled($out, $png, 180, 180, 0, 0, $newwidth, $newheight, $newwidth, $newheight);
 		imagejpeg($out, $_GET['img'], 100);
 	}
 	else {
@@ -18,7 +18,7 @@
 		$width = 480;
 		$height = 480;
 		$out = imagecreatetruecolor($width, $height);
-		imagecopyresampled($out, $jpeg, 0, 0, 0, 0, $width, $height, $width, $height);
+		imagecopyresampled($out, $jpeg, 0, 0, 100, 0, $width, $height, $width, $height);
 		imageflip($out, IMG_FLIP_HORIZONTAL);
 		imagejpeg($out, $_GET['img'], 100);
 	}

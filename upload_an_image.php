6d95a6fb620f0bd7c82a5ddf78a942732d831803
Mabouce <mabouce@student.42.fr>
2019-10-23 15:53:19 +0200

    better css for upoad an image

--- upload_an_image.php
+++ upload_an_image.php
@@ -15,57 +15,20 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Upload une image</title>
-		<style>
-		.parent_div_container {
-			position: relative;
-			display: flex;
-			align-content: stretch;
-			width: 80%;
-		}
-		.child-upload-1,
-		.child-upload-2 {
-			position: relative;
-			overflow: hidden;
-			width: 50%;
-		}
-		.upload-div {
-			position: relative;
-			overflow: hidden;
-			width: 480px;
-			height: 480px;
-			border: solid 1px black;
-		}
-		.filtre {
-			position: absolute;
-			width: 100px;
-			height: 100px;
-			right: calc(50% - 50px);
-			top: calc(50% - 50px);
-			z-index: 1;
-		}
-		.btn-upload {
-			background-color: rgb(36, 156, 212);
-			color: white;
-			padding: 14px 20px;
-			margin: 8px 0;
-			margin-top: 1px;
-			border: none;
-			border-radius: 10%;
-			cursor: pointer;
-			width: 20%;
-			font-size: 13px;
-		}
-		</style>
+		<link rel="stylesheet" type="text/css" href="./css/upload_an_image.css">
 	<head>
 	<body>
 		<?php include("menu.php") ?>
+		<br>
+		<br>
+		<br>
 		<form action="phpfunctions/upload_file.php" method="post" enctype="multipart/form-data">
 			Select image to upload:
 			<input type="file" name="fileToUpload" id="fileToUpload" required>
 			<input type="submit" value="Upload Image" name="submit">
 		</form>
 		<?php if (isset($_SESSION['error'])) {
-					echo '<p style="text-align:center">' . $_SESSION['error'] . '</p>';}?>
+					echo '<p id="text">' . $_SESSION['error'] . '</p>';}?>
 		<?php if (isset($_SESSION['img_uploaded'])) {
 		?>
 		<div class="parent_div_container">
@@ -75,7 +38,7 @@
 					<img style="display:none ;" class="filtre" id="img-preview-in-upload">
 					<img id="img_upload" src="./ressources/uploads/<?=$_SESSION['img_uploaded']?>">
 				</div>
-				<button class="btn-upload" onclick="myAjaxSendToGalerylocal()" value="Upload Image" id="save-upload">Save to galery</button>
+				<button class="btn-camera" onclick="myAjaxSendToGalerylocal()" value="Upload Image" id="save-upload">Save to galery</button>
 			</div>
 			<div class="child-upload-2">
 				<center>
